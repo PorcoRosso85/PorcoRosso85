0eb54cea3291c8f218ac5e1a9ebdd7cc6a120838
PorcoRosso85 <1.is.universe@gmail.com>
2024-01-19 15:03:03 +0900

    Add extractCommentBlock function and modify main function

--- lib/statemodel/src/main.ts
+++ lib/statemodel/src/main.ts
@@ -2,19 +2,37 @@ import { SQLComment } from './types'
 import { blockFormatFunction } from './blockFormat'
 import { extractCommentBlock } from './extractCommentBlock'
 import fs from 'fs'
+import { promisify } from 'util'
 
-export function main(state: any, filePath: string) {
+export async function main(state: any, filePath: string) {
   const comments: SQLComment[] = extractCommentBlock(state)
 
   const blockFormat = ['BEGIN------------------------------', '------------------------------END']
 
-  for (const comment of comments) {
-    // commentの値がundefinedの場合、空文字に変換する
-    comment.description = comment.description || ''
-    const formattedCommentBlock = blockFormatFunction(comment, blockFormat)
-    // [] 照合機能: 既存ファイルに、フォーマット通りのコメントブロックが存在するか
-    fs.existsSync(filePath)
-      ? fs.appendFileSync(filePath, formattedCommentBlock)
-        fs.writeFileSync(filePath, formattedCommentBlock)
+  const readFile = promisify(fs.readFile)
+  const appendFile = promisify(fs.appendFile)
+
+  try {
+    const newData = []
+    if (fs.existsSync(filePath)) {
+      const data = await readFile(filePath, 'utf8')
+      for (const comment of comments) {
+        comment.description = comment.description || ''
+        const formattedCommentBlock = blockFormatFunction(comment, blockFormat)
+        // formattedCommentBlockを使用した処理をここに書く
+        newData.push(formattedCommentBlock)
+      }
+    } else {
+      fs.writeFileSync(filePath, '')
+      for (const comment of comments) {
+        comment.description = comment.description || ''
+        const formattedCommentBlock = blockFormatFunction(comment, blockFormat)
+        newData.push(formattedCommentBlock)
+      }
+    }
+
+    appendFile(filePath, newData.join('\n'), 'utf8')
+  } catch (err) {
+    console.error(err)
   }
 }
