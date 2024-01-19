76b46137d50759aff033d6b3850ef32d8173fe6f
PorcoRosso85 <1.is.universe@gmail.com>
2024-01-19 13:46:00 +0900

    if undefined, insert emplty

--- lib/statemodel/src/main.ts
+++ lib/statemodel/src/main.ts
@@ -9,9 +9,12 @@ export function main(state: any, filePath: string) {
   const blockFormat = ['BEGIN------------------------------', '------------------------------END']
 
   for (const comment of comments) {
+    // commentの値がundefinedの場合、空文字に変換する
+    comment.description = comment.description || ''
     const formattedCommentBlock = blockFormatFunction(comment, blockFormat)
+    // [] 照合機能: 既存ファイルに、フォーマット通りのコメントブロックが存在するか
     fs.existsSync(filePath)
       ? fs.appendFileSync(filePath, formattedCommentBlock)
-      : fs.writeFileSync(filePath, formattedCommentBlock)
+        fs.writeFileSync(filePath, formattedCommentBlock)
   }
 }
